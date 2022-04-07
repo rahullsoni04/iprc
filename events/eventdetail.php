7e03ac20e533b7d5666f19188133d29362fad50c
Israil Alam <52991105+Mdizrail92@users.noreply.github.com>
2022-04-08 01:05:33 +0530

    event details page

--- events/eventdetail.php
+++ events/eventdetail.php
@@ -17,6 +17,83 @@ require_once '../requirements.php';
 ?>
 
 <body>
+    <!-- Navbar -->
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-dark">
+        <div class="container-fluid">
+            <div class="logo">
+                <a href="../index.php">
+                    <img src="../images/IPR logo.png">
+                </a>
+            </div>&nbsp; &nbsp;
+            <a class="navbar-brand" href="../index.php">SAKEC IPR</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php#about">About</a>
+                    </li>
+                    <li class="nav-item ">
+                        <?php 
+                            if ((isset($_SESSION['email'])&& ($_SESSION['email']==="a@sakec.ac.in"))){
+                                echo '<a class="nav-link " href="event_management.php">Events Management</a>';
+                            } 
+                        ?>
+                    </li>
+                    <li class="nav-item ">
+                    <?php 
+                            if ((isset($_SESSION['email'])&& ($_SESSION['email']==="a@sakec.ac.in"))){
+                                echo '<a class="nav-link " href="../admin/landing.php">Edit Landing</a>';
+                            } else {
+                                echo '<a class="nav-link " href="../index.php#roles">Our Team</a>';
+                            }
+                        ?>
+                    </li>
+                    <li class="nav-item ">
+                    <?php 
+                            if ((isset($_SESSION['email'])&& ($_SESSION['email']==="a@sakec.ac.in"))){
+                                echo '<a class="nav-link " href="../query.php">Query</a>';
+                            }
+                        ?>
+                    </li>
+                    <li class="nav-item ">
+                    <?php 
+                            if ((isset($_SESSION['email'])&& ($_SESSION['email']==="a@sakec.ac.in"))){
+                                echo '<a class="nav-link " href="../roles.php">Edit Roles</a>';
+                            }
+                        ?>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="#footer">
+                            Contact Us
+                        </a>
+                    </li>
+
+                </ul>
+                <div class="d-flex justify-content-center">
+                    <?php
+                    if (!isset($_SESSION['email'])) {
+                    ?>
+                        <a href="../sign-up.php" class="button" style=" padding: 5px 15px; color: white;">Login</a>
+                    <?php
+                    } else {
+                    ?>
+                        <a href="../logout.php" class="button" style=" padding: 5px 15px; color: white;">Logout</a>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div style="height: 60px;"></div>
+
+    <!-- navbar ends -->
   <?php
   if (!isset($_GET['id'])) {
     header("Location: events.php");
@@ -31,102 +108,27 @@ require_once '../requirements.php';
     $event_row = mysqli_fetch_assoc($query);
   }
   ?>
-
-  <!-- banner -->
   <div id="detail">
     <div class="content">
-      <div class="main-text">
+      <div class="main-text mt-4">
         <h1><?php echo $event_row['title']; ?></h1>
-        <p>Event 2021-2022</p>
       </div>
     </div>
   </div>
-  <!-- banner ends -->
+  <!-- banner -->
+  <div class="container text-center">
+    <div class="row">
 
-  <!-- Event Details section -->
-  <div class="spacer" style="height: 15px;"></div>
-  <section>
-    <div class="about-content text-left">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-lg-4 col-md-6">
-            <br>
-            <p>
-              <i class="fas fa-calendar-alt"></i>
-              <span class="event_date">&nbsp; <?php echo date('j F Y', strtotime($event_row['from_date'])) . " to " . date('j F Y', strtotime($event_row['to_date'])); ?> </span>
-              <br><br>
-              <i class="fas fa-clock"></i>&nbsp;
-              <?php echo date('h:i', strtotime($event_row['from_time'])) . " - " . date('h:i', strtotime($event_row['to_time'])); ?>
-            </p>
-            <div class="spacer" style="height: 20px;"></div>
-            <h4 class="mt-6">For any Queries :</h4>
-            <?php
-            // fetching from coordinator table
-            $sql = 'SELECT * FROM `ipr_event_coordinator` WHERE `event_id`= ' . $id;
-            $query = mysqli_query($conn, $sql);
-            if (!mysqli_num_rows($query)) {
-              echo '<h3>No Records Found</h3>';
-            } else {
-              while ($event_coordinator_row = mysqli_fetch_assoc($query)) {
-            ?>
-                <p class="my-4"><?php echo $event_coordinator_row['name']; ?> - <i class="fas fa-phone"></i>&nbsp;<?php echo $event_coordinator_row['contact']; ?> </p>
-            <?php
-              }
-            }
-            ?>
-          </div>
-          <div class="col-sm-12 col-lg-4 col-md-6"><br>
-          </div>
-          <div class="col-sm-12 col-lg-4 col-md-6">
-            <br><br>
-            <img src="data:image/jpg;base64,'<?php echo base64_encode($event_row['banner']) ?>'">
-            <img src="data:image/jpg;base64">
-            <br>
-            <div class="spacer" style="height: 20px;"></div>
-            <a class="register" type="submit">Register</a>
-          </div>
-        </div>
-        <br>
-        <hr class="new-row">
-      </div>
-      <div class="spacer" style="height: 40px;"></div>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-12 col-lg-8">
-            <h4>Event Overview</h4>
-            <p class="my-4">
-              <?php echo $event_row['description']; ?>
-            </p>
-            <div class="spacer" style="height: 20px;"></div>
-            <hr>
-            <p><strong>Venue : </strong> <?php echo $event_row['venue']; ?></p>
-            <strong>Follow Us : </strong> &nbsp;
-            <i class="fab fa-facebook-square"></i>&nbsp; &nbsp;
-            <i class="fab fa-instagram"></i>&nbsp; &nbsp;
-            <i class="fab fa-linkedin"></i>&nbsp; &nbsp;
-            <i class="fab fa-youtube"></i>
-          </div>
-          <div class="col-sm-12 col-md-12 col-lg-4">
-            <!-- popup image -->
-            <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-              <div class="modal-dialog modal-xl modal-dialog-centered">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Event Banner
-                    </h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <?php
-                    echo '<img  src="src="../images/IPR_fb.jpg""  alt="banner" class="banner-popup" >';
-                    ?>
-                  </div>
-                </div>
-              </div>
-            </div>
+  
+<div class="col-sm-7">
 
-            <?php
+<?php echo '<img src="data:image/jpeg;base64,' . base64_encode($event_row['banner']) . '"  data-toggle="modal" data-target="#staticBackdrop" alt="" class="img-fluid image-banner" > '; ?>
+
+
+</div>
+<div class="col-sm-5">
+
+<?php
             // fetching from event table
             $sql = 'SELECT * FROM `ipr_speakers` WHERE `event_id`=' . $id;
             $query = mysqli_query($conn, $sql);
@@ -135,7 +137,7 @@ require_once '../requirements.php';
             ?>
 
               <!-- Modal ends -->
-              <div class="speaker-info text-center">
+              <div class="row text-center speaker-info ">
                 <h4 class="mt-4">Speaker For the Event</h4>
                 <div class="responsive-slides">
                   <?php
@@ -160,13 +162,136 @@ require_once '../requirements.php';
                 </div>
                 <div class="spacer" style="height: 20px;"></div>
               </div>
+</div>
+
+
+  </div>
+  
+  </div>
+
+
+  <!-- Event Details section -->
+  <div class="spacer" style="height: 15px;"></div>
+  <section>
+    <div class="about-content text-left">
+      <div class="container top-event">
+        <div class="row">
+          <div class="col-sm-12 col-lg-4 col-md-5">
+            <br>
+            <p>
+              <i class="fas fa-calendar-alt"></i>
+              <span class="event_date">&nbsp; <?php echo date('j F Y', strtotime($event_row['from_date'])) . " to " . date('j F Y', strtotime($event_row['to_date'])); ?> </span>
+              <br><br>
+              <i class="fas fa-clock"></i>&nbsp;
+              <?php echo date('h:i', strtotime($event_row['from_time'])) . " - " . date('h:i', strtotime($event_row['to_time'])); ?>
+            </p>
+            <div class="spacer" style="height: 20px;"></div>
+            <h4 class="mt-6">For any Queries :</h4>
+            <?php
+            // fetching from coordinator table
+            $sql = 'SELECT * FROM `ipr_event_coordinator` WHERE `event_id`= ' . $id;
+            $query = mysqli_query($conn, $sql);
+            if (!mysqli_num_rows($query)) {
+              echo '<h3>No Records Found</h3>';
+            } else {
+              while ($event_coordinator_row = mysqli_fetch_assoc($query)) {
+            ?>
+                <p class="my-4"><?php echo $event_coordinator_row['name']; ?> - <i class="fas fa-phone"></i>&nbsp;<?php echo $event_coordinator_row['contact']; ?> </p>
+            <?php
+              }
+            }
+            ?>
+            
+          </div>
+          
+         
+       
+          <div class="col-sm-12 col-lg-8 col-md-7">
+          <h4>Event Overview</h4>
+            <p class="my-4">
+              <?php echo $event_row['description']; ?>
+            </p>
+            <div class="spacer" style="height: 20px;"></div>
+            <a class="button event-datail" type="submit">Register</a>
+            <hr>
+            <p><strong>Venue : </strong> <?php echo $event_row['venue']; ?></p>
+            <strong>Follow Us : </strong> &nbsp;
+            <i class="fab fa-facebook-square"></i>&nbsp; &nbsp;
+            <i class="fab fa-instagram"></i>&nbsp; &nbsp;
+            <i class="fab fa-linkedin"></i>&nbsp; &nbsp;
+            <i class="fab fa-youtube"></i>
           </div>
         </div>
+        <br>
+        <hr class="new-row">
       </div>
-  </section>
-  <hr>
+      <div class="spacer" style="height: 25px;"></div>
   <!-- Event Details ends -->
+<!-- Footer -->
+<footer class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 ">
+                    <h2>Our College</h2>
+                    <hr>
+                    <p><i class="fas fa-map-marker-alt"></i> Mahavir Education Trust Chowk, W.T Patil Marg, D P Rd, next to Duke's Company, Chembur, Mumbai, Maharashtra 400088</p>
+                    <p><i class="fas fa-phone"></i> 022-25580854, 022-67978100</p>
+                    <p><i class="fas fa-envelope"></i> ipr@sakec.ac.in</p>
+                </div>
+                <div class="col-sm-3">
+                    <h2>Quick Links</h2>
+                    <hr>
+                    <p>ipr@sakec.ac.in</p>
+                    <p>ipr@sakec.ac.in</p>
+                    <p>ipr@sakec.ac.in</p>
+                    <p>ipr@sakec.ac.in</p>
+                </div>
+                <div class="col-sm-3">
+                    <h2>Recent News and Courses</h2>
+                    <hr>
+                    <p>ipr@sakec.ac.in</p>
+                    <p>ipr@sakec.ac.in</p>
+                    <p>ipr@sakec.ac.in</p>
+                    <p>ipr@sakec.ac.in</p>
+                    <p>ipr@sakec.ac.in</p>
+                </div>
+                <div class="col-sm-3">
+                    <h2>Get In Touch For Any Query</h2>
+                    <hr>
 
+                    <form>
+                        <div class="mb-3 form-group">
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name">
+                        </div>
+                        <div class="mb-3 form-group">
+                            <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email">
+                        </div>
+                        <div class="mb-3 form-group">
+                            <!-- <textarea name="message" id="" placeholder="Your query"></textarea> -->
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Your Query"></textarea>
+                        </div>
+                        <button type="submit" class="mb-2 btnp">Send Message</button>
+                    </form>
+                </div>
+            </div>
+
+            <hr>
+            <div class="copyright">
+                <div class="row justify-content-between">
+                    <div class="col-4">
+                        © IPR SAKEC, All Right Reserved.
+                    </div>
+                    <div class="col-4 text-center">
+                        Follow Us : &nbsp
+                        <i class="fab fa-facebook-square"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-linkedin"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
